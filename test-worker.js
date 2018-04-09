98cb9c5da95c1a00dfe96edc36c29601be2bacd1
Fernando Giovanini <giovanini.fernando@gmail.com>
2018-04-09 08:40:23 +0200

    Fixed test-worker

--- test-worker.js
+++ test-worker.js
@@ -2,15 +2,18 @@
  * Use it for testing the worker directly without having to wait for the scheduler to run
  */
 const mongodb = require('./src/backend/mongodb');
-const { databaseName } = require('./src/config');
 
 const setup = async () => {
-    const client = await mongodb();
-    const db = client.db(databaseName);
-
+    try {
+        await mongodb.connect();
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    
     const worker = require('./src/backend/worker');
 
-    worker(client, db, true, true);
+    worker(true, true);
 };
 
 setup();
